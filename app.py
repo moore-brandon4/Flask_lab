b447920ce6f88361e3c9b2c95401a66289a1fdff
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-09-10 04:29:06 +0000

    flask lab

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request           # import flask framework
+app = Flask(__name__)             # create an app instance
+
+@app.route("/")                
+def hello():                    
+    return render_template("index.html") 
+if __name__ == "__main__":        # when running python app.py
+    app.run(debug=True)                     # run the flask app
+
+
+@app.route("/<name>")              # route with URL variable /<name>
+def hello_name(name):              # call method hello_name
+    return "Hello "+ name          # which returns "hello + name  
+
+@app.route("/about")
+def about():
+    name = request.args.get('name') if request.args.get('name') else "Hello World!" 
+
+    return render_template("about.html", aboutName=name) 
